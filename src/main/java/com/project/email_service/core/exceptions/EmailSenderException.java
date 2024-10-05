346bcd780c03e7b51d124feb647291144092965b
Diego Fabbri <diegofabbri.dev@gmail.com>
2024-10-05 01:23:18 -0300

    feat: add EmailSenderException class

---
+++ src/main/java/com/project/email_service/core/exceptions/EmailSenderException.java
@@ -0,0 +1,11 @@
+package com.project.email_service.core.exceptions;
+
+public class EmailSenderException extends RuntimeException{
+    public EmailSenderException(String message) {
+        super(message);
+    }
+
+    public EmailSenderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
